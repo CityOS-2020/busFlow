8b306c84637e3bfd48251374afe70bc1d5e6d412
MrNiceMrGood <mrnicemrgood@gmail.com>
2015-03-15 15:20:05 +0100

    Marker added

--- busFlow/RouteViewController.swift
+++ busFlow/RouteViewController.swift
@@ -16,17 +16,26 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
     
     @IBOutlet weak var mapView: MKMapView!
     var routeCoordinates:[CLLocationCoordinate2D] = []
-    var pressedLocation:CLLocation?
+    
     var busses:[PathObject] = []
-    var currentLocation:CLLocation?
     let urlBusses = "http://flowbus.eu-gb.mybluemix.net/api/busses"
     let urlBusStations = "http://flowbus.eu-gb.mybluemix.net/api/stations"
     var busStations:[PathObject] = []
     var annotations:[CustomPointAnnotation]=[]
     let locationManager = CLLocationManager()
-    var distanceFromUserToNearestStation:CLLocationDistance?
-    var distanceFromDestinationToNearestStation:CLLocationDistance?
-    var userLocations:[CLLocation] = []
+    var markerAnnotation:CustomPointAnnotation?
+    
+    var userLocation:CLLocation?
+    
+    var userNearestStation:PathObject?
+    var destinationNearestStation:PathObject?
+    
+    var busOfInterest:PathObject?
+    
+    var estimatedTimeToDestination:Int?
+    var estimatedBusTimeArrival:Int?
+    
+    var destination:CLLocation?
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(false)
@@ -39,16 +48,15 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         //just for presentation, hardcoded user location  because we cant update user location
         let spanX = 0.01
         let spanY = 0.01
-        var hardcodedLocation:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 43.8470823, longitude: 18.3741403)
-        var newRegion = MKCoordinateRegion(center: hardcodedLocation, span: MKCoordinateSpanMake(spanX, spanY))
+        let hardcodedLocation:CLLocation = CLLocation(latitude: 43.8470823, longitude: 18.3741403)
+        self.userLocation = hardcodedLocation
+        var newRegion = MKCoordinateRegion(center: hardcodedLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
         var annotation = MKPointAnnotation()
         mapView.addAnnotation(annotation)
-        annotation.coordinate = hardcodedLocation
-
+        annotation.coordinate = hardcodedLocation.coordinate
         
-    
         mapView.setRegion(newRegion, animated: true)
-    
+        
         mapView.delegate = self
         mapView.mapType = MKMapType.Hybrid
         if (CLLocationManager.locationServicesEnabled()){
@@ -62,14 +70,13 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         longPress.minimumPressDuration = 1.0
         self.mapView.addGestureRecognizer(longPress)
         longPress.addTarget(self, action: "longPressToGetLocation:")
-
+        
         //parsing route coordinates from file
         let bundle = NSBundle.mainBundle()
         let path = bundle.pathForResource("Komercijala", ofType: "txt")
         let content = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil)
         let filteredContent = content!.stringByReplacingOccurrencesOfString(",0.0", withString: "", options: nil, range: nil)
         let arrayOfCoordinates = filteredContent.componentsSeparatedByString(" ")
-        println(arrayOfCoordinates)
         
         for index in 0..<arrayOfCoordinates.count{
             var longlat = arrayOfCoordinates[index].componentsSeparatedByString(",")
@@ -80,6 +87,7 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         
         //setting mapView to match route
         var route = MKPolyline(coordinates: &self.routeCoordinates, count:  self.routeCoordinates.count)
+        route.title = "sss"
         mapView.addOverlay(route)
         
         
@@ -87,12 +95,58 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         var helloWorldTimer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "refresh", userInfo: nil, repeats: true)
         getBusStations()
     }
+    
+    
+    //functions for distance calculations and time estmations
+    func findNearestStationFromLocation(location:CLLocationCoordinate2D)->(PathObject, CLLocationDistance){
+        
+        return findNearestFromLocation(location, pathObjects: self.busStations)
+    }
+    
+    func estimatedArrivelTime(pathObject1:PathObject,pathObject2:PathObject)->Int{
+        return (pathObject2.pathIndex! - pathObject1.pathIndex!)*3
+    }
+    
+    func findNearestBusFromStation(busStation:PathObject)->(PathObject, CLLocationDistance, Int){
+        var buses = self.busses.filter({s in s.pathIndex < busStation.pathIndex})
+        println(busStation)
+        var resultTuple  = findNearestFromLocation(busStation.coordinates!,  pathObjects: self.busses)
+        return (resultTuple.0,resultTuple.1, (busStation.pathIndex! - resultTuple.0.pathIndex!)*3)
+    }
+    
+    func findNearestFromLocation(location:CLLocationCoordinate2D, pathObjects:[PathObject]) -> (PathObject, CLLocationDistance) {
+        
+        var locationOfInterest = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        
+        println(locationOfInterest)
+        
+        var busStationLocation = CLLocation(latitude: pathObjects[0].coordinates!.latitude, longitude: pathObjects[0].coordinates!.longitude)
+        var minDistance = locationOfInterest.distanceFromLocation(busStationLocation)
+        var nearestIndex = 0
+        
+        for i in 1..<pathObjects.count {
+            busStationLocation = CLLocation(latitude: pathObjects[i].coordinates!.latitude, longitude:  pathObjects[i].coordinates!.longitude)
+            
+            var distance = locationOfInterest.distanceFromLocation(busStationLocation)
+            
+            println(distance)
+            
+            if (distance < minDistance) {
+                nearestIndex = i
+                minDistance = distance
+            }
+        }
+        
+        return(pathObjects[nearestIndex], minDistance)
+    }
+    
+    
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
         if !(annotation is CustomPointAnnotation) {
             return nil
         }
         
-        let reuseIconId = "busIcon"
+        let reuseIconId = "bus"
         var annotationView = self.mapView.dequeueReusableAnnotationViewWithIdentifier(reuseIconId)
         if annotationView == nil {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIconId)
@@ -102,48 +156,80 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
             annotationView.annotation = annotation
         }
         let busAnnotation = annotation as CustomPointAnnotation
-        annotationView.image = UIImage(named:busAnnotation.imageName!)
+        if(busAnnotation.imageName != nil){
+            annotationView.image = UIImage(named:busAnnotation.imageName!)
+        }else{
+            annotationView.image = UIImage(named:"bus")
+        }
+        
         return annotationView
     }
 
     
     func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
         
-        self.userLocations.append(locations[0] as CLLocation)
-        let spanX = 0.007
-        let spanY = 0.007
-        if( self.userLocations.count == 1){
-        var newRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
-        mapView.setRegion(newRegion, animated: true)
-        }
+        //self.userLocation = locations[0] as? CLLocation
+             updatePOIS()
     }
-
-
     
     func longPressToGetLocation(gestureRecognizer:UILongPressGestureRecognizer){
         if(gestureRecognizer.state != UIGestureRecognizerState.Began){
-            var pressPoint:CGPoint = gestureRecognizer.locationInView(self.mapView)
+            
+            var pressPoint:CGPoint = gestureRecognizer .locationInView(self.mapView)
             var chosenLocation:CLLocationCoordinate2D = self.mapView.convertPoint(pressPoint, toCoordinateFromView: self.mapView)
-            self.pressedLocation = CLLocation(latitude: chosenLocation.latitude, longitude: chosenLocation.latitude)
+            self.destination = CLLocation(latitude: chosenLocation.latitude, longitude: chosenLocation.longitude)
+            self.mapView.removeAnnotation(self.markerAnnotation)
             var annotation = CustomPointAnnotation()
             annotation.coordinate = chosenLocation
+            annotation.imageName = "destination"
             self.mapView.addAnnotation(annotation)
-            annotation.imageName = "busStop"
-
-                    }
+            self.markerAnnotation = annotation
+            updatePOIS()
+     
+            
+        }
+    }
+    
+    func updatePOIS(){
+        if( self.destination != nil && self.userLocation != nil){
+            println(self.destination)
+            self.destinationNearestStation = self.findNearestStationFromLocation(self.destination!.coordinate).0
+            var resTouple = self.findNearestBusFromStation(self.userNearestStation!)
+            (self.busOfInterest, self.estimatedBusTimeArrival) = (resTouple.0,resTouple.2)
+            self.estimatedTimeToDestination = estimatedArrivelTime(self.busOfInterest!, pathObject2: self.destinationNearestStation!)
+            var coord = [CLLocationCoordinate2D]()
+            coord.append(self.userNearestStation!.coordinates!)
+            coord.append(self.destinationNearestStation!.coordinates!)
+            
+            
+            var route = MKPolyline(coordinates: &coord, count:  coord.count)
+            route.title = "rt"
+            mapView.addOverlay(route)
+            
+        } else{
+            
+        }
     }
     
     
-
     func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
         if overlay is MKPolyline {
             var polylineRenderer = MKPolylineRenderer(overlay: overlay)
-            polylineRenderer.strokeColor = UIColor(red: 71/256, green: 191/256, blue: 209/256, alpha: 1)
-            polylineRenderer.lineWidth = 4
-            return polylineRenderer
+
+            if (overlay.title != nil && overlay.title == "rt"){
+                polylineRenderer.strokeColor = UIColor(red: 134/256, green: 191/256, blue: 209/256, alpha: 0.5)
+                polylineRenderer.lineWidth = 8
+                
+            }else {
+                polylineRenderer.strokeColor = UIColor(red: 71/256, green: 191/256, blue: 209/256, alpha: 1)
+                polylineRenderer.lineWidth = 5
+
+            }
+
+                        return polylineRenderer
         }
         return nil
-    }
+        }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -156,21 +242,23 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
                     NSLog("Error: \(error)")
                 }
                 else {
-                   // NSLog("Success: \(self.urlBusStations)")
+                    // NSLog("Success: \(self.urlBusStations)")
                     var jsonBusStations = JSON(json!).arrayValue
+                    
                     for i in jsonBusStations {
                         self.busStations.append(PathObject(pathObject:i))
                     }
-
+                    
                     for index in 0..<self.busStations.count{
                         var annotation = CustomPointAnnotation()
                         annotation.coordinate = self.routeCoordinates[self.busStations[index].pathIndex!]
                         self.mapView.addAnnotation(annotation)
                         annotation.imageName = "station"
-                        UIView.animateWithDuration(0.0, animations: { () -> Void in
-                        })
                     }
+                    println(self.userLocation)
+                    self.userNearestStation = self.findNearestStationFromLocation(self.userLocation!.coordinate).0
                 }
+                
         }
     }
     
@@ -181,7 +269,7 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
                     NSLog("Error: \(error)")
                 }
                 else {
-                   // NSLog("Success: \(self.urlBusses)")
+                    // NSLog("Success: \(self.urlBusses)")
                     var jsonBusses = JSON(json!).arrayValue
                     self.busses.removeAll(keepCapacity: true)
                     for i in jsonBusses {
@@ -202,11 +290,19 @@ class RouteViewController: UIViewController, MKMapViewDelegate, CLLocationManage
                     }
                     else{
                         for index in 0..<self.busses.count{
+                            
+                            if(self.busses[index].id == self.busOfInterest?.id){
+                                self.annotations[index].imageName = "bus-of-interest-icon"
+                            }else{
+                                self.annotations[index].imageName = "bus"
+                            }
+                            
                             self.annotations[index].coordinate =  self.routeCoordinates[self.busses[index].pathIndex!]
                             UIView.animateWithDuration(3.0, animations: { () -> Void in
                                 self.annotations[index].coordinate = self.routeCoordinates[(self.busses[index].pathIndex! + 1)%620]
                             })
                         }
+                        self.updatePOIS()
                     }
                 }
         }
